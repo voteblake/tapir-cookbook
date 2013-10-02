276170d7a3f21ac4ec1865f06d1dc02268824927
Blake Johnson <blakejohnson@alliantenergy.com>
2013-10-01 21:30:20 -0500

    add packages from tapir install instructions

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,4 +7,40 @@
 # All rights reserved - Do Not Redistribute
 #
 
+package "nmap" 
+	action :install
+end
+
+package "qt4-qmake" 
+	action :install
+end
+
+package "libnokogiri-ruby1.8" 
+	action :install
+end
+
+package "libxslt-dev" 
+	action :install
+end
+
+package "libxml2-dev" 
+	action :install
+end
+
+package "libqt4-dev" 
+	action :install
+end
+
+package "libpcap-dev" 
+	action :install
+end
+
+package "libpq-dev" 
+	action :install
+end
+
+package "libsqlite3-dev" 
+	action :install
+end
+
 include_recipe "mongodb::default"
